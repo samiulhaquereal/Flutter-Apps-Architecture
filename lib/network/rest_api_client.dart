374c3aadc51a3e03bfc8016919f40ca13e20643b
B.M.Samiul Haque Real <samiulreal198@gmail.com>
2024-02-16 10:18:17 +0600

    3rd

--- lib/network/rest_api_client.dart
+++ lib/network/rest_api_client.dart
@@ -7,96 +7,140 @@ import 'endpoints_string.dart';
 // Get -> List , Map
 // Put -> List , Map
 // Post -> List , Map
-Map<String, String> _buildHeaders({Map<String, String>? additionalHeaders}) {
-  if (additionalHeaders != null) {
-    Map<String, String> headers = {
-      'Content-Type': 'application/json',
-      ...additionalHeaders,
-    };
-    return headers;
-  } else {
-    return {
-      'Content-Type': 'application/json',
-    };
-  }
-}
 
-void _printRequest(http.Response response) {
-  print('Request: ${response.request}');
-  print('Status code: ${response.statusCode}');
-  print('Headers: ${response.headers}');
-  print('Body: ${response.body}');
-}
+class RestApi {
 
-Future<Map<String, dynamic>> getMap({required String endpoint, Map<String, dynamic>? parms}) async {
-  EasyLoading.show(status: 'loading...');
-
-  try {
-    final uri = Uri.parse('$baseUrl$endpoint');
-    final response = await http.get(
-      uri.replace(queryParameters: parms),
-      headers: _buildHeaders(),
-    );
-    _printRequest(response);
-    if (response.statusCode == 200) {
-      final decodedResponse = json.decode(response.body);
-      if (decodedResponse is Map<String, dynamic> &&
-          decodedResponse.isNotEmpty) {
-        EasyLoading.dismiss();
-        return decodedResponse;
-      } else {
-        EasyLoading.showError("Error occurred while making GET Request");
-        throw Exception('Unsupported type: ${decodedResponse.runtimeType}');
-      }
+  Map<String, String> _buildHeaders({Map<String, String>? additionalHeaders}) {
+    if (additionalHeaders != null) {
+      Map<String, String> headers = {
+        'Content-Type': 'application/json',
+        ...additionalHeaders,
+      };
+      return headers;
     } else {
+      return {
+        'Content-Type': 'application/json',
+      };
+    }
+  }
+
+  void _printRequest(http.Response response) {
+    print('Request: ${response.request}');
+    print('Status code: ${response.statusCode}');
+    print('Headers: ${response.headers}');
+    print('Body: ${response.body}');
+  }
+
+  Future<Map<String, dynamic>> getMap({required String endpoint, Map<String, dynamic>? parms}) async {
+    EasyLoading.show(status: 'loading...');
+
+    try {
+      final uri = Uri.parse('$baseUrl$endpoint');
+      final response = await http.get(
+        uri.replace(queryParameters: parms),
+        headers: _buildHeaders(),
+      );
+      _printRequest(response);
+      if (response.statusCode == 200) {
+        final decodedResponse = json.decode(response.body);
+        if (decodedResponse is Map<String, dynamic> &&
+            decodedResponse.isNotEmpty) {
+          EasyLoading.dismiss();
+          return decodedResponse;
+        } else {
+          EasyLoading.showError("Error occurred while making GET Request");
+          throw Exception('Unsupported type: ${decodedResponse.runtimeType}');
+        }
+      } else {
+        if (kDebugMode) {
+          print(
+              'Request failed when status code is not 200 with status: ${response.statusCode}');
+        }
+        EasyLoading.showError("Error occurred while making GET Request");
+        throw Exception('Failed to make GET request');
+      }
+    } catch (e) {
       if (kDebugMode) {
-        print(
-            'Request failed when status code is not 200 with status: ${response.statusCode}');
+        print('Error occurred while making GET request: $e');
       }
       EasyLoading.showError("Error occurred while making GET Request");
-      throw Exception('Failed to make GET request');
+      throw Exception('Failed to make GET request: $e');
     }
-  } catch (e) {
-    if (kDebugMode) {
-      print('Error occurred while making GET request: $e');
-    }
-    EasyLoading.showError("Error occurred while making GET Request");
-    throw Exception('Failed to make GET request: $e');
   }
-}
 
-Future<List> getList({required String endpoint, Map<String, dynamic>? params}) async {
-  EasyLoading.show(status: 'Loading...');
-  try {
-    final uri = Uri.parse('$baseUrl$endpoint');
-    final response = await http.get(
-      uri.replace(queryParameters: params),
-      headers: _buildHeaders(),
-    );
-    _printRequest(response);
-    if (response.statusCode == 200) {
-      final decodedResponse = json.decode(response.body);
-      if (decodedResponse is List<dynamic> && decodedResponse.isNotEmpty) {
-        EasyLoading.dismiss();
-        return decodedResponse;
+  Future<List> getList({required String endpoint, Map<String, dynamic>? params}) async {
+    EasyLoading.show(status: 'Loading...');
+    try {
+      final uri = Uri.parse('$baseUrl$endpoint');
+      final response = await http.get(
+        uri.replace(queryParameters: params),
+        headers: _buildHeaders(),
+      );
+      _printRequest(response);
+      if (response.statusCode == 200) {
+        final decodedResponse = json.decode(response.body);
+        if (decodedResponse is List<dynamic> && decodedResponse.isNotEmpty) {
+          EasyLoading.dismiss();
+          return decodedResponse;
+        } else {
+          EasyLoading.showError("Error occurred while making GET Request");
+          throw Exception('Unsupported type: ${decodedResponse.runtimeType}');
+        }
       } else {
+        if (kDebugMode) {
+          print(
+              'Request failed when status code is not 200 with status: ${response.statusCode}');
+        }
         EasyLoading.showError("Error occurred while making GET Request");
-        throw Exception('Unsupported type: ${decodedResponse.runtimeType}');
+        throw Exception('Failed to make GET request');
       }
-    } else {
+    } catch (e) {
       if (kDebugMode) {
-        print(
-            'Request failed when status code is not 200 with status: ${response.statusCode}');
+        print('Error occurred while making GET request: $e');
       }
       EasyLoading.showError("Error occurred while making GET Request");
-      throw Exception('Failed to make GET request');
+      throw Exception('Failed to make GET request: $e');
     }
-  } catch (e) {
-    if (kDebugMode) {
-      print('Error occurred while making GET request: $e');
-    }
-    EasyLoading.showError("Error occurred while making GET Request");
-    throw Exception('Failed to make GET request: $e');
   }
+
+  Future<Map<String, dynamic>> postMap({required String endpoint, Map<String, dynamic>? parms}) async {
+    EasyLoading.show(status: 'loading...');
+
+    try {
+      final uri = Uri.parse('$baseUrl$endpoint');
+      final response = await http.get(
+        uri.replace(queryParameters: parms),
+        headers: _buildHeaders(),
+      );
+      _printRequest(response);
+      if (response.statusCode == 200) {
+        final decodedResponse = json.decode(response.body);
+        if (decodedResponse is Map<String, dynamic> &&
+            decodedResponse.isNotEmpty) {
+          EasyLoading.dismiss();
+          return decodedResponse;
+        } else {
+          EasyLoading.showError("Error occurred while making GET Request");
+          throw Exception('Unsupported type: ${decodedResponse.runtimeType}');
+        }
+      } else {
+        if (kDebugMode) {
+          print(
+              'Request failed when status code is not 200 with status: ${response.statusCode}');
+        }
+        EasyLoading.showError("Error occurred while making GET Request");
+        throw Exception('Failed to make GET request');
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error occurred while making GET request: $e');
+      }
+      EasyLoading.showError("Error occurred while making GET Request");
+      throw Exception('Failed to make GET request: $e');
+    }
+  }
+
 }
+
+
 
---
+++ lib/network/services.dart
@@ -0,0 +1,23 @@
+import 'package:mvc_format/network/endpoints_string.dart';
+
+import 'rest_api_client.dart';
+
+class Services {
+  RestApi _restApiClient = RestApi();
+
+  Future<Map<String, dynamic>?> getTask({required int projectId}) async {
+    final response = await _restApiClient.getMap(endpoint: endpoint_name);
+    print('Printing response');
+    print(response);
+    return response;
+  }
+
+  Future<List> getNameList() async {
+    final response = await _restApiClient.getList(endpoint: endpoint_name);
+    print('Printing response');
+    print(response);
+    return response;
+  }
+
+
+}
